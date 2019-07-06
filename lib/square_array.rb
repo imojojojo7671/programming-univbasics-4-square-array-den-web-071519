f72a16014be71da3d99fcffab4447247e4f5ab4b
Drew Peterson <drew.a.peterson@hotmail.com>
2019-07-06 16:00:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   count = 0
   new_square_array = []
   while array(counter) do
-    new_square_array << array[counter] ** array[counter]
+    new_square_array << array[counter] ** 2
     count += 1
   end
   new_square_array
